84c70c93be114e522e84cefa4a2c32cb419f0002
estefanoquiriconi <estefanoquiriconi@gmail.com>
2025-03-03 22:58:37 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { PatientForm } from './components'
+import { PatientsList } from './components'
+
+function App() {
+  return (
+    <>
+      <div className='container mx-auto mt-20'>
+        <h1 className='font-black text-5xl text-center md:w-2/3 md:mx-auto'>
+          Seguimiento de pacientes {''}
+          <span className='text-indigo-700'>Veterinaria</span>
+        </h1>
+
+        <div className='mt-12 md:flex'>
+          <PatientForm />
+          <PatientsList />
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
